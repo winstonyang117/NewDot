8b48f3438bc14f6dfd5478a095ab187b82c0d89d
winstonyang117 <winstonyang117@gmail.com>
2022-09-27 21:21:10 +0100

    Updated run.sh(for crontab service)

--- run.sh
+++ run.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 now=$(date +"%T")
 SERVICE="python3"
+
+sudo /usr/bin/python3 /home/pi/NewDot/test.py
+ip_wlan0=$(ip addr show wlan0|grep inet|grep -v inet6|awk '{print $2}'|awk '{split($0,a,"/"); print a[1]}')
+serial_port="/dev/ttyS0"
+sudo /usr/bin/python3 /home/pi/NewDot/seismoBridge.py $serial_port
+sudo /usr/bin/python3 /home/pi/BDotAI/src/algtest_noRaw.py $ip_wlan0
+
+
 if pgrep -x "$SERVICE" >/dev/null
 then
     echo "$SERVICE is running at $now" > /home/pi/NewDot/log.txt
 else
     echo "$SERVICE stopped at $now" > /home/pi/NewDot/log.txt
-    nohup sudo /usr/bin/python3 /home/pi/NewDot/serialClient_final.py /dev/ttyS0 > /home/pi/NewDot/log.txt &
+    #nohup sudo /usr/bin/python3 /home/pi/NewDot/serialClient_final.py /dev/ttyS0 > /home/pi/NewDot/log.txt &
+
 fi
---
+++ test.py
@@ -0,0 +1,2 @@
+print("************** Hello! *****************")
+print("************** If you can see this message pop up, the fun.sh file is successfully running!  *****************")
