d8cecf4fc4de980b8fb942da4436b0e9911eef50
winstonyang117 <winstonyang117@gmail.com>
2022-10-20 21:04:17 +0100

    updated run.sh

--- run.sh
+++ run.sh
@@ -2,20 +2,23 @@
 now=$(date +"%T")
 SERVICE="python3"
 
-sudo /usr/bin/python3 /home/pi/NewDot/test.py
+# sudo /usr/bin/python3 /home/pi/NewDot/test.py
 ip_wlan0=$(ip addr show wlan0|grep link/ether|grep -v inet6|awk '{print $2}'|awk '{split($0,a,"/"); print a[1]}')
 serial_port="/dev/ttyS0"
-sudo /usr/bin/python3 /home/pi/NewDot/serialClient_final.py $serial_port &
-sudo /usr/bin/python3 /home/pi/BDotAI/src/algtest_noRaw.py $ip_wlan0
+nohup sudo /usr/bin/python3 /home/pi/NewDot/serialClient_final.py $serial_port 
+# nohup sudo /usr/bin/python3 /home/pi/NewDot/serialClient_final.py $serial_port &
+# sudo /usr/bin/python3 /home/pi/BDotAI/src/algtest_noRaw.py $ip_wlan0
 
 # shell script will not execute the next command unless it finishes the current one, that's why we use "&" to let them run together. 
 
 
-if pgrep -x "$SERVICE" >/dev/null
-then
-    echo "$SERVICE is running at $now" > /home/pi/NewDot/log.txt
-else
-    echo "$SERVICE stopped at $now" > /home/pi/NewDot/log.txt
-    #nohup sudo /usr/bin/python3 /home/pi/NewDot/serialClient_final.py /dev/ttyS0 > /home/pi/NewDot/log.txt &
+# if pgrep -x "$SERVICE" >/dev/null
+# then
+#     echo "$SERVICE is running at $now" > /home/pi/NewDot/log.txt
+# else
+#     echo "$SERVICE stopped at $now" > /home/pi/NewDot/log.txt
+#     #nohup sudo /usr/bin/python3 /home/pi/NewDot/serialClient_final.py /dev/ttyS0 > /home/pi/NewDot/log.txt &
+#     nohup sudo /usr/bin/python3 /home/pi/NewDot/serialClient_final.py $serial_port &
+#     sudo /usr/bin/python3 /home/pi/BDotAI/src/algtest_noRaw.py $ip_wlan0
 
-fi
+# fi
